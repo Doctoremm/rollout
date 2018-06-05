3a6e1172eab2ccdfebc148fb14f8cc40f40fc954
Anthony Romrell <anthony@artisandream.com>
2018-06-05 09:30:18 -0600

    General Updates

---
+++ TransformGame/Assets/AssetSearch.cs
@@ -0,0 +1,23 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "AssetSearch")]
+public class AssetSearch : ScriptableObject
+{
+
+	[MenuItem("GameObject/Create Material")]
+	
+	void OnEnable()
+	{
+		
+		Material material = new Material(Shader.Find("Standard"));
+		
+		//AssetDatabase.CreateAsset(material, "Assets/Other.mat");
+		//AssetDatabase.CreateFolder("Assets", "NewFolder");
+		AssetDatabase.DeleteAsset("Assets/New.mat");
+		AssetDatabase.SaveAssets();
+		
+		// Print the path of the created asset
+		Debug.Log(AssetDatabase.GetAssetPath(material));
+	}
+}
